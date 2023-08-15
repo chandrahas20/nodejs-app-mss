6eca66b55e4fe5e41bfebd29f56048a820f947bb
chandrahas20 <89960588+chandrahas20@users.noreply.github.com>
2023-08-15 12:47:30 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.213.15.220:9000/',
+  serverUrl: 'http://18.143.94.111:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_c0b7a8d9ee35cd07573af2b1f3a47aa12bfa9634',
+	    'sonar.login': 'squ_ebc7b48dbb0d64698998183fc660f0a929f9b090',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
